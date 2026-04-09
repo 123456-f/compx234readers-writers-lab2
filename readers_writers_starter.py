da397dcaacb979c143983898106837cb617ce4bc
123456-f <385655160@qq.com>
2026-04-09 09:38:41 +0800

    task change5

--- readers_writers_starter.py
+++ readers_writers_starter.py
@@ -176,20 +176,27 @@ def main() -> None:
 
     #TODO: Create at least 3 Reader threads.
     readers = [
-        Reader(reader_id=1, monitor=monitor)
+        Reader(reader_id=1, monitor=monitor),
+        Reader(reader_id=2, monitor=monitor),
+        Reader(reader_id=3, monitor=monitor)
     ]
     
     #TODO: Create at least 2 writer threads.
     writers = [
-        Writer(writer_id=1, monitor=monitor)
+        Writer(writer_id=1, monitor=monitor),
+        Writer(writer_id=2, monitor=monitor)
     ]
 
     all_threads = readers + writers
     
     # TODO: Start all threads
+    for t in all_threads:
+        t.start()
 
     
     # TODO: Wait for all threads to finish
+    for t in all_threads:
+        t.join()
 
 
     # TODO: Print final message that simulation completed
